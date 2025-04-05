33f9a7e45c4f5eea7c9ed357a655360544d3a67f
Mauricio Barg <mrwbarg@gmail.com>
2025-04-05 17:56:15 -0300

    Implements init command

--- cmd/init.go
+++ cmd/init.go
@@ -10,9 +10,16 @@ func init() {
 }
 
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "Initialize a new git repository",
+	Use:   "init [path]",
+	Short: "Initialize a new git repository in the given path. If no path is provided, the current directory will be used.",
+	Args:  cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		commands.InitCmd()
+		var path string
+		if len(args) == 0 {
+			path = "./"
+		} else {
+			path = args[0]
+		}
+		commands.InitCmd(path)
 	},
 }
--- internal/commands/init.go
+++ internal/commands/init.go
@@ -1,12 +1,9 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/mrwbarg/go-git-clone/internal/models/repository"
 )
 
-func InitCmd() {
-	fmt.Println("Initializing a new git repository")
-	repository.Initialize("./")
+func InitCmd(path string) {
+	repository.Initialize(path)
 }
